212bdfda86d7322262b2c8409bddbcc790d68a59
Liam Sargent <liam@ziptech.io>
2020-05-11 15:21:55 +0000

    Added docker-compose

--- sessionstore.py
+++ sessionstore.py
@@ -31,10 +31,9 @@ class SessionStore(object):
     def __init__(self, flush=True):
         self._data = {}
         self._next_slot = 1 # eventually implement a pointer here for atomic seeks
-        self._rejson = Client(host='192.168.86.24',
+        self._rejson = Client(host='cloudloop-rejson',
                               port=6379,
                               decode_responses=True,
-                              password='cloudloop',
                               encoder=LoopEncoder(),
                               decoder=LoopDecoder(),
                               db=1)
