c11efab3e9a8108bf7bc9a308d908dcb4ab43c0b
Delivator <info@delivator.me>
2020-11-10 17:46:51 +0100

    Addes pages Session and Sessions

---
+++ src/views/Session.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="4">
+        <v-btn x-large fab dark large color="black">
+          <v-icon dark>
+            mdi-android
+          </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Session",
+  data() {
+    return {
+      data: require("../../example-data.json")
+    };
+  }
+};
+</script>
---
+++ src/views/Sessions.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container class="my-10">
+    <v-rows>
+      <v-col cols="12">
+        <span class="text-h3 text-uppercase font-weight-bold">
+          Your Sessions
+          <v-btn icon color="black" @click="isPrivate = !isPrivate">
+            <v-icon>{{ isPrivate ? "lock" : "lock_open" }}</v-icon>
+          </v-btn>
+        </span>
+      </v-col>
+    </v-rows>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Sessions",
+  data() {
+    return {
+      isPrivate: true
+    };
+  }
+};
+</script>
