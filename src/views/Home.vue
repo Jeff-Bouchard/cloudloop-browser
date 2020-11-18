91e2bf332892be17a0c0f84cd1457a573f1ea548
Delivator <info@delivator.me>
2020-11-18 20:48:26 +0100

    Added welcome back message to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,8 +11,11 @@
       </v-col>
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
+        <h1 v-if="!loggedInUser" class="display-2 font-weight-bold mb-3">
           Welcome to cloudloop
+        </h1>
+        <h1 v-else class="display-2 font-weight-bold mb-3">
+          Welcome back {{ loggedInUser }}
         </h1>
 
         <p class="subheading font-weight-regular">
@@ -53,6 +56,12 @@ export default {
         to: "sessions"
       }
     ]
-  })
+  }),
+
+  computed: {
+    loggedInUser() {
+      return this.$store.state.loggedInUser;
+    }
+  }
 };
 </script>
