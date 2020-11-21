0b43755a428bc71fcf7771898d79ed3861a9276e
Liam Sargent <themailman05@gmail.com>
2020-11-21 10:29:53 -0600

    Remove history

--- .history/src/filters/utils_20201120110811.js
+++
@@ -1,7 +0,0 @@
-export const getDownloadLink = siaLink => {
-  if (siaLink.startsWith("sia://")) {
-    return "https://siasky.net/" + siaLink.substring(5);
-  } else {
-    return siaLink;
-  }
-};
