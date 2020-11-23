0ec9d30e54b3cceaaf63178fe01bc696431876f8
Sam Wellander <samwellander@gmail.com>
2020-11-22 18:18:46 -0800

    Add fetch user method to store

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,8 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     loggedInUser: null,
-    selectedSession: null
+    selectedSession: null,
+    selectedProfile: null,
   },
   mutations: {
     setLoggedInUser(state, value) {
@@ -14,12 +15,39 @@ export default new Vuex.Store({
     },
     setSelectedSession(state, value) {
       state.selectedSession = value;
+    },
+    setSelectedProfile(state, value) {
+      state.selectedProfile = value;
     }
   },
   actions: {
     setSelectedSession({ commit }, payload) {
       console.log("Selecting session: " + payload.session.name);
       commit("setSelectedSession", payload.session);
+    },
+    fetchUserProfile({ commit }, userName) {
+      return new Promise((resolve, reject) => {
+        const fetchOptions = {
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `JWT ${window.localStorage.getItem("JWT")}`
+          }
+        };
+  
+        fetch(
+            "https://dev.cloudloop.io/user?username=" + userName,
+            fetchOptions
+        ).then(response => {
+            if (response.ok) return response.json()
+            else reject(response.json().message);
+          })
+          .then(({data: {results}}) => {
+            console.log({results})
+            commit('setSelectedProfile', results);
+            resolve(results);
+          })
+      });
     },
     logInUser({ commit }, userPass) {
       return new Promise((resolve, reject) => {
