e805cfeaaf931733cae47ab84d49235d60b21ee5
Sam Wellander <samwellander@gmail.com>
2020-11-22 18:17:22 -0800

    Add progress bar when loading waveforms

--- src/components/WaveformPlayer.vue
+++ src/components/WaveformPlayer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid>
-     <v-row justify="center">
+    <v-row justify="center">
       <v-col align-self="center" cols="12" lg="2">
         <div style="text-align: end">
           <v-btn
@@ -19,34 +19,38 @@
           </v-btn>
         </div>
       </v-col>
-      <v-col class="waveform-wrapper" cols="12" lg="10">
+      <v-col class="waveform-wrapper" cols="12" lg="10" align-self="center">
         <v-icon
           medium
           @click="toggleStar"
           class="icon star-icon"
           :style="isStarred && 'color: gold'"
         >
-          {{isStarred ? 'star' : 'mdi-star-outline'}}
+          {{ isStarred ? "star" : "mdi-star-outline" }}
         </v-icon>
-        <v-icon
-          medium
-          @click="downloadLoop"
-          class="icon download-icon"
-        >
+        <v-icon medium @click="downloadLoop" class="icon download-icon">
           mdi-download-circle
         </v-icon>
+        <v-progress-linear
+          class="percent-loaded"
+          v-if="percentLoaded < 100"
+          :value="percentLoaded"
+          color="#3b2898"
+          background-color="#7cecdc"
+          height="2"
+        ></v-progress-linear>
         <div :id="'waveform-' + loop.hash"></div>
         <p class="wave-tag creator-tag">
           <span>
             <strong>CREATOR: </strong>
           </span>
-          <span class="tag-value">{{loop.creator}}</span>
+          <span class="tag-value">{{ loop.creator }}</span>
         </p>
         <p class="wave-tag created-on-tag">
           <span>
             <strong>CREATED ON: </strong>
           </span>
-          <span class="tag-value">{{loop.created_at | formatDate}}</span>
+          <span class="tag-value">{{ loop.created_at | formatDate }}</span>
         </p>
       </v-col>
     </v-row>
@@ -54,104 +58,118 @@
 </template>
 
 <style scoped>
-  .waveform-wrapper {
-    position: relative;
-  }
-  .wave-tag {
-    position: absolute;
-    font-size: 12px;
-  }
-  .creator-tag {
-    right: 45%;
-  }
-  .created-on-tag {
-    right: 12%;
-  }
-  .tag-value {
-    font-size: 12px;
-    color: #646464;
-  }
-  .icon {
-    position: absolute;
-    z-index: 3;
-  }
-  .star-icon {
-    right: 6%;
-  }
-  .download-icon {
-    right: 0;
-    color: black;
-  }
+.percent-loaded {
+  position: absolute;
+  top: 50px;
+}
+.waveform-wrapper {
+  position: relative;
+}
+.wave-tag {
+  position: absolute;
+  font-size: 12px;
+}
+.creator-tag {
+  right: 45%;
+}
+.created-on-tag {
+  right: 12%;
+}
+.tag-value {
+  font-size: 12px;
+  color: #646464;
+}
+.icon {
+  position: absolute;
+  z-index: 3;
+}
+.star-icon {
+  right: 6%;
+}
+.download-icon {
+  right: 0;
+  color: black;
+}
 </style>
 
 <script>
-  import WaveSurfer from "wavesurfer.js";
-  import {getDownloadLink} from "@/filters/utils";
+import WaveSurfer from "wavesurfer.js";
+import { getDownloadLink } from "@/filters/utils";
 
-  export default {
-    name: "WaveformPlayer",
-    props: ["loop"],
-    data() {
-      return {
-        isPlaying: false,
-        isStarred: false
-      }
+export default {
+  name: "WaveformPlayer",
+  props: ["loop"],
+  data() {
+    return {
+      isPlaying: false,
+      isStarred: false,
+      percentLoaded: 0,
+    };
+  },
+  mounted() {
+    const { loop } = this.$props;
+
+    this.waveSurfer = WaveSurfer.create({
+      container: `#waveform-${loop.hash}`,
+      barWidth: 2,
+      barHeight: 3,
+      barMinHeight: 1,
+      cursorWidth: 1,
+      backend: "WebAudio",
+      height: 80,
+      progressColor: "#3b2898",
+      responsive: true,
+      waveColor: "#7cecdc",
+      cursorColor: "transparent",
+    });
+
+    const downloadLink = getDownloadLink(loop.link);
+    this.waveSurfer.load(downloadLink);
+
+    // Loop tracks
+    this.waveSurfer.on("finish", () => {
+      this.waveSurfer.play();
+    });
+    this.waveSurfer.on("loading", (percent) => {
+      this.percentLoaded = percent;
+    });
+  },
+
+  methods: {
+    downloadLoop() {
+      const downloadLink = getDownloadLink(this.$props.loop.link);
+      location.href = downloadLink;
     },
-    mounted() {
-      const { loop } = this.$props;
-
-      this.waveSurfer = WaveSurfer.create({
-        container: `#waveform-${loop.hash}`,
-        barWidth: 2,
-        barHeight: 3,
-        barMinHeight: 1,
-        cursorWidth: 1,
-        backend: "WebAudio",
-        height: 80,
-        progressColor: "#3b2898",
-        responsive: true,
-        waveColor: "#7cecdc",
-        cursorColor: "transparent",
-      })
-
-      const downloadLink = getDownloadLink(loop.link);
-      this.waveSurfer.load(downloadLink);
-
-      // Loop tracks
-      this.waveSurfer.on('finish', () => {
-        this.waveSurfer.play();
-      })
+    toggleStar() {
+      this.isStarred = !this.isStarred;
     },
-
-    methods: {
-      downloadLoop() {
-        const downloadLink = getDownloadLink(this.$props.loop.link)
-        location.href = downloadLink;
-      },
-      toggleStar() {
-        this.isStarred = !this.isStarred;
-      },
-      playPause() {
-        this.waveSurfer.playPause();
-        this.isPlaying = !this.isPlaying;
-      },
-      pause() {
-        this.waveSurfer.pause();
-        this.isPlaying = false;
-      },
-      play() {
-        this.waveSurfer.play();
-        this.isPlaying = true;
-      },
-      playFromStart() {
-        this.waveSurfer.play(0);
-      }
+    playPause() {
+      this.waveSurfer.playPause();
+      this.isPlaying = !this.isPlaying;
     },
-    filters: {
-      formatDate(dateStr) {
-        const dateObj = new Date(dateStr);
-        return dateObj.getFullYear() + "-" + (dateObj.getMonth() + 1) + "-" + dateObj.getDate(); 
-      }
-    }
-  };
+    pause() {
+      this.waveSurfer.pause();
+      this.isPlaying = false;
+    },
+    play() {
+      this.waveSurfer.play();
+      this.isPlaying = true;
+    },
+    playFromStart() {
+      this.waveSurfer.play(0);
+    },
+  },
+  filters: {
+    formatDate(dateStr) {
+      const dateObj = new Date(dateStr);
+      return (
+        dateObj.getFullYear() +
+        "-" +
+        (dateObj.getMonth() + 1) +
+        "-" +
+        dateObj.getDate()
+      );
+    },
+  },
+};
 </script>
