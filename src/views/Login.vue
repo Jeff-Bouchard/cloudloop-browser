134b7b4aad07c4caf263da50fdae184fdd403db0
Liam Sargent <themailman05@gmail.com>
2020-12-02 08:44:01 -0600

    push user to profile page on login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -73,7 +73,7 @@ export default {
         })
         .then(() => {
           this.loading = false;
-          this.$router.push("/");
+          this.$router.push(`/users/${this.username}`);
         })
         .catch(error => {
           this.loading = false;
